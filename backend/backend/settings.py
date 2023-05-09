4902adf648a4d909c6c2be0a7152123b105659ba
Egor <Egor@email.com>
2023-05-09 13:55:26 +0000

    load

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.100.0', '127.0.0.1', 'localhost', '1one1one1.hopto.org']
 
 
 # Application definition
@@ -103,7 +103,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# Замените стандартное значение 'static' на 'static_backend',
+# чтобы не было конфликта запросов к статике фронтенда и бэкенда.
+STATIC_URL = 'static_backend'
+# Укажите директорию, куда бэкенд-приложение должно сложить статику.
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
