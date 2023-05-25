87ae8d8cdb5860e49d939410c8ff9309f7c79e9d
Прылипко Егор <Egorprylipko@mail.ru>
2023-05-25 18:12:44 +0300

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,8 +62,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # Меняем настройку Django: теперь для работы будет использоваться
+        # бэкенд postgresql
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
@@ -105,9 +112,9 @@ USE_TZ = True
 
 # Замените стандартное значение 'static' на 'static_backend',
 # чтобы не было конфликта запросов к статике фронтенда и бэкенда.
-STATIC_URL = 'static_backend'
+STATIC_URL = '/static/'
 # Укажите директорию, куда бэкенд-приложение должно сложить статику.
-STATIC_ROOT = BASE_DIR / 'static_backend' 
+STATIC_ROOT = BASE_DIR / 'collected_static' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
